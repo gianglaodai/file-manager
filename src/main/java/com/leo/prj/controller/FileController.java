d599d7f1b4173eaca192466d277f0ed6a2229544
giang <gianghoang107@gmail.com>
2018-02-28 01:05:26 +0700

    update upload methods

--- src/main/java/com/leo/prj/controller/FileController.java
+++ src/main/java/com/leo/prj/controller/FileController.java
@@ -1,8 +1,30 @@
 package com.leo.prj.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.leo.prj.bean.UploadFilesResult;
+import com.leo.prj.service.UploadService;
 
 @RestController
 public class FileController {
-
+	@Autowired
+	private UploadService uploadService;
+	
+	@PostMapping("/uploadFiles")
+	public ResponseEntity<UploadFilesResult> uploadFiles(@RequestParam("files")List<MultipartFile> uploadFiles){
+		return ResponseEntity.ok(uploadService.uploadFiles(uploadFiles));
+	}
+	
+	@PostMapping("/uploadFile")
+	public ResponseEntity<UploadFilesResult> uploadFiles(@RequestParam("file")MultipartFile uploadFile) throws Exception{
+		return ResponseEntity.ok(uploadService.uploadFile(uploadFile));
+	}
 }
--- src/main/java/com/leo/prj/service/UploadService.java
+++ src/main/java/com/leo/prj/service/UploadService.java
@@ -1,5 +1,6 @@
 package com.leo.prj.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.tika.Tika;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.common.net.MediaType;
 import com.leo.prj.bean.UploadFilesResult;
 
 @Service
@@ -24,9 +26,11 @@ public class UploadService {
 		this.tika = tika;
 	}
 
-	public UploadFilesResult uploadFile(MultipartFile uploadFile) {
+	public UploadFilesResult uploadFile(MultipartFile uploadFile) throws Exception {
 		final UploadFilesResult uploadFilesResult = this.uploadFilesResult;
-
+		final MediaType mediaType = MediaType.parse(this.tika.detect(uploadFile.getBytes()));
+		if(mediaType.is(MediaType.APPLE_MOBILE_CONFIG)) {
+		}
 		return uploadFilesResult;
 	}
 
