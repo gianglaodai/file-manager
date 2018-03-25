7ba1339d6ac28246b8e7211d4afb899846f8ee10
gianglaodai <gianghoang107@gmail.com>
2018-03-26 02:30:42 +0700

    fix compile error

--- src/main/java/com/leo/prj/service/page/PageService.java
+++ src/main/java/com/leo/prj/service/page/PageService.java
@@ -3,10 +3,15 @@ package com.leo.prj.service.page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.leo.prj.bean.EditorPageData;
 import com.leo.prj.service.UserService;
 
 @Service
 public class PageService {
 	@Autowired
 	private UserService userService;
+
+	public boolean savePage(EditorPageData page) {
+		return true;
+	}
 }
